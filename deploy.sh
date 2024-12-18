01781947ebcfd21e89781268a504a69014f7a537
ryardley <contact@rudiyardley.com>
2024-12-18 13:22:26 +1100

    Add some nice things to script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,29 @@
 #!/usr/bin/env bash
-docker stack rm p2p && sleep 20
-docker stack deploy -c docker-compose.yml --prune p2p && sleep 10
+wait_ready() {
+    local stack_name="$1"
+    until [ "$(docker stack services $stack_name --format '{{.Replicas}}' | awk -F'/' '$1 != $2')" = "" ]; do
+        printf "."
+        sleep 1
+    done
+    echo -ne "\r\033[K"
+    echo "Stack $stack_name is ready!"
+}
+
+wait_removed() {
+  local stack_name="$1"
+
+  while docker stack ps $stack_name >/dev/null 2>&1; do
+      printf "."
+      sleep 1
+  done
+  echo -ne "\r\033[K"
+  echo "Stack $stack_name is removed"
+}
+
+docker stack rm p2p
+
+wait_removed p2p
+
+docker stack deploy -c docker-compose.yml --prune p2p
+
+wait_ready p2p
