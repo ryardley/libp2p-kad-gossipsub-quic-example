dad710057f55565db46dcd7f245d89b59cb09728
ryardley <contact@rudiyardley.com>
2024-12-19 14:23:14 +1100

    Update to use service id to only show most recent deployments

--- inspect.sh
+++ inspect.sh
@@ -1,21 +1,38 @@
 #!/usr/bin/env bash
+
+get_logs_by_version() {
+    local SERVICE_NAME=$1
+    
+    # Get current version number
+    CURRENT_VERSION=$(docker service inspect --format '{{.Version.Index}}' $SERVICE_NAME)
+    
+    # Get all tasks with this version
+    TASK_IDS=$(docker service ps --filter "desired-state=running" \
+        --format '{{.ID}}' $SERVICE_NAME)
+    
+    # Get logs from these specific tasks
+    for TASK_ID in $TASK_IDS; do
+        docker service logs --raw "$TASK_ID"
+    done
+}
+
 echo ""
 echo "================================="
 echo "           BOOTSTRAP "
 echo "================================="
 
-docker service logs p2p_bootstrap
+get_logs_by_version p2p_bootstrap
 
 echo ""
 echo "================================="
 echo "           PEER "
 echo "================================="
-docker service logs p2p_peer
+get_logs_by_version p2p_peer
 
 
 echo ""
 echo "================================="
 echo "           SENDER "
 echo "================================="
-docker service logs p2p_sender
+get_logs_by_version p2p_sender
 
