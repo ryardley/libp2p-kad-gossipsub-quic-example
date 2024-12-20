b3ae7e9265732d66f2e3afd2da0d2555b11a52d2
ryardley <contact@rudiyardley.com>
2024-12-20 14:38:46 +1100

    Try with ufw

--- firewall_setup.sh
+++ firewall_setup.sh
@@ -1,32 +1,10 @@
 #!/usr/bin/env bash
 
-sudo mkdir -p /etc/iptables
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow 22/tcp
+sudo ufw allow 4001/udp
+sudo ufw allow 4002/udp
+sudo ufw allow 4003/udp
 
-# First, flush existing rules
-iptables -F
-iptables -X
-iptables -Z
-
-# Set default policies to DROP
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
-iptables -P OUTPUT ACCEPT
-
-# Allow loopback traffic
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-
-# Allow established and related connections
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-# Allow specific ports (both TCP and UDP)
-iptables -A INPUT -p tcp -m multiport --dports 4001,4002,4003 -j ACCEPT
-iptables -A INPUT -p udp -m multiport --dports 4001,4002,4003 -j ACCEPT
-
-# If you need SSH access (recommended), add:
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-
-# For Debian/Ubuntu:
-iptables-save > /etc/iptables/rules.v4
-# For RHEL/CentOS:
-# service iptables save
+sudo ufw enable
